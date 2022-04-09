7520359d5f19055eea8e6c98bd3d6502a25262a8
Aurange <auragar@users.noreply.github.com>
2022-04-09 15:45:36 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,18 @@
 //Created by Aurange
 
-let scale = document.getElementById("scale"), times = document.getElementById("times"), inElem = document.getElementById("in"), br = document.getElementsByTagName("br")[0], message = document.getElementById("message"), can = document.getElementById("canvas"), ctx = can.getContext("2d");
+let scaleElem = document.getElementById("scale"), timesElem = document.getElementById("times"), inElem = document.getElementById("in"), brElem = document.getElementsByTagName("br")[0], messageElem = document.getElementById("message"), canElem = document.getElementById("canvas"), ctx = canElem.getContext("2d");
 
-document.getElementById("scale").onkeypress = function(e){
+scaleElem.onkeypress = function(e){
   e.preventDefault();
 };
 
-document.getElementById("scale").oninput = function(){
-  if(document.getElementById("in").style.display !== "inline") document.getElementById("in").style.display = "inline";
-  if(document.getElementById("canvas").style.display !== "none") document.getElementById("canvas").style.display = "none";
+scaleElem.oninput = function(){
+  if(inElem.style.display !== "inline") inElem.style.display = "inline";
+  if(canElem.style.display !== "none") canElem.style.display = "none";
 }
 
-document.getElementById("in").onchange = function(){
-  let sVal = Number(scale.value), file = new FileReader();
+inElem.onchange = function(){
+  let sEVal = Number(scaleElem.value), file = new FileReader();
 
   file.onload = function(){
     let input = new Image();
@@ -22,32 +22,32 @@ document.getElementById("in").onchange = function(){
     input.onload = function(){
       let colors, len, oWidth = input.width, upscaled = [], storage = [];
 
-      can.width = oWidth;
-      can.height = input.height;
+      canElem.width = oWidth;
+      canElem.height = input.height;
 
       ctx.drawImage(input, 0, 0);
 
-      colors = ctx.getImageData(0, 0, can.width, can.height).data;
+      colors = ctx.getImageData(0, 0, canElem.width, canElem.height).data;
 
       len = colors.length;
 
-      can.width *= sVal;
-      can.height *= sVal;
+      canElem.width *= sEVal;
+      canElem.height *= sEVal;
 
-      scale.style.display = "none";
-      times.style.display = "none";
-      br.style.display = "none";
+      scaleElem.style.display = "none";
+      timesElem.style.display = "none";
       inElem.style.display = "none";
-      message.style.display = "block";
+      brElem.style.display = "none";
+      messageElem.style.display = "block";
 
       for(let i = 0, j = 1; i < len; i += 4, ++j){
-        for(let k = 0; k < sVal; ++k){
+        for(let k = 0; k < sEVal; ++k){
           upscaled.push(colors[i], colors[i + 1], colors[i + 2], colors[i + 3]);
           storage.push(colors[i], colors[i + 1], colors[i + 2], colors[i + 3]);
         }
 
         if(j % oWidth === 0){
-          for(let k = 1; k < sVal; ++k){
+          for(let k = 1; k < sEVal; ++k){
             upscaled.push(...storage);
           }
 
@@ -55,19 +55,19 @@ document.getElementById("in").onchange = function(){
         }
       }
 
-      ctx.putImageData(new ImageData(Uint8ClampedArray.from(upscaled), can.width), 0, 0);
+      ctx.putImageData(new ImageData(Uint8ClampedArray.from(upscaled), canElem.width), 0, 0);
 
-      message.style.display = "none";
-      scale.style.display = "inline";
-      times.style.display = "inline";
-      br.style.display = "inline";
-      can.style.display = "block";
+      messageElem.style.display = "none";
+      scaleElem.style.display = "inline";
+      timesElem.style.display = "inline";
+      brElem.style.display = "inline";
+      canElem.style.display = "block";
     }
   }
 
   file.readAsDataURL(inElem.files[0]);
 
-  scale.value = null;
+  scaleElem.value = null;
   inElem.value = null;
 
   inElem.blur();
